0aa0af47b1ad9816cabac6971a4fd3cf3fa027fc
Brayan_Torres <bbrayato@amazon.com>
2023-03-10 09:18:09 -0500

    pipeline add

--- lib/multiaccountdeploy-stack.ts
+++ lib/multiaccountdeploy-stack.ts
@@ -1,16 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MultiaccountdeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'Sme-Demo',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('brayiyandres/pipelineSME', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MultiaccountdeployQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  
   }
 }
